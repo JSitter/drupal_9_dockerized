d59f766a47ad8270295cd3c459d3abb51c05cae5
Justin Sitter <justin@jaytria.com>
2020-09-18 22:49:59 -0700

    Add Drupal core 9.0.6

--- drupal-src/vendor/autoload.php
+++ drupal-src/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit8f9f146e666740836b40d9f2ce183d81::getLoader();
+return ComposerAutoloaderInite35b93403106a07b297654780d2e6bc9::getLoader();
